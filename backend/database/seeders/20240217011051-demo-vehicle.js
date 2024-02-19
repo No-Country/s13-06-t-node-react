54d34d6eef4b6b34af93ff8a860be53505fe7ab5
varayac <victor.d.araya@gmail.com>
2024-02-19 15:47:09 -0300

    fix(backend): Modify seed files for vehicle models and amenities
     On branch 43-crear-modelos-de-vehicle-y-amenities
     Your branch is up to date with 'origin/43-crear-modelos-de-vehicle-y-amenities'.
    
     Changes to be committed:
    	modified:   database/seeders/20240217011051-demo-vehicle.js
    	modified:   database/seeders/20240217011139-demo-amenity.js

--- backend/database/seeders/20240217011051-demo-vehicle.js
+++ backend/database/seeders/20240217011051-demo-vehicle.js
@@ -8,9 +8,54 @@ module.exports = {
       'Vehicles',
       [
         {
+          number: 1,
+          plate: 'QRST90',
+          totalSeats: 60
+        },
+        {
+          number: 2,
+          plate: 'ABCD12',
+          totalSeats: 40
+        },
+        {
+          number: 3,
+          plate: 'WXYZ34',
+          totalSeats: 40
+        },
+        {
+          number: 4,
+          plate: 'PQRS56',
+          totalSeats: 60
+        },
+        {
+          number: 5,
+          plate: 'JKLM78',
+          totalSeats: 40
+        },
+        {
+          number: 6,
+          plate: 'UVWX90',
+          totalSeats: 60
+        },
+        {
+          number: 7,
+          plate: 'STUV12',
+          totalSeats: 40
+        },
+        {
+          number: 8,
+          plate: 'EFGH34',
+          totalSeats: 60
+        },
+        {
+          number: 9,
+          plate: 'IJKL56',
+          totalSeats: 40
+        },
+        {
           number: 10,
-          plate: 'RJPF62',
-          totalSeats: 42
+          plate: 'MNOP78',
+          totalSeats: 40
         }
       ],
       {}
--- backend/database/seeders/20240217011139-demo-amenity.js
+++ backend/database/seeders/20240217011139-demo-amenity.js
@@ -11,6 +11,80 @@ module.exports = {
           idVehicle: 1,
           usb: true,
           tv: true,
+          food: false,
+          wifi: true,
+          airConditioner: true
+        },
+        {
+          idVehicle: 2,
+          usb: true,
+          tv: true,
+          food: true,
+          wifi: true,
+          airConditioner: true
+        },
+        {
+          idVehicle: 3,
+          usb: true,
+          tv: true,
+          food: true,
+          wifi: true,
+          airConditioner: true
+        },
+        {
+          idVehicle: 4,
+          usb: true,
+          tv: true,
+          food: false,
+          wifi: true,
+          airConditioner: true
+        },
+        {
+          idVehicle: 5,
+          usb: true,
+          tv: true,
+          food: true,
+          wifi: true,
+          airConditioner: true
+        },
+        {
+          idVehicle: 6,
+          usb: true,
+          tv: true,
+          food: false,
+          wifi: true,
+          airConditioner: true
+        },
+        {
+          idVehicle: 7,
+          usb: true,
+          tv: true,
+          food: true,
+          wifi: true,
+          airConditioner: true
+        },
+        {
+          idVehicle: 8,
+          usb: true,
+          tv: true,
+          food: false,
+          wifi: true,
+          airConditioner: true
+        },
+        {
+          idVehicle: 9,
+          usb: true,
+          tv: true,
+          food: true,
+          wifi: true,
+          airConditioner: true
+        },
+        {
+          idVehicle: 10,
+          usb: true,
+          tv: true,
+          food: true,
+          wifi: true,
           airConditioner: true
         }
       ],
