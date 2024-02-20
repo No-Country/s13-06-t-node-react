e543dc890a2ee2bfb13902ace8c26dcf7607c371
redmor1 <lightz17@hotmail.com>
2024-02-20 14:57:21 -0300

    style(frontend): Modify styles to look like design

--- frontend/src/components/Itinerary/ItinerarySchedule.jsx
+++ frontend/src/components/Itinerary/ItinerarySchedule.jsx
@@ -13,7 +13,7 @@ function ItinerarySchedule(props) {
       <div className='flex justify-between font-medium text-[#486284] text-[15px] opacity-90 tracking-tight mb-1'>
         <h3>{props.departingDay}</h3> <h3>{props.arrivalDay}</h3>
       </div>
-      <div className='flex justify-between font-medium text-xl tracking-tight items-center text-[#1A202C] mb-28'>
+      <div className='flex justify-between font-medium text-xl tracking-tight items-center text-[#1A202C]'>
         <time dateTime={props.departingHour}>{props.departingHour}</time>
         <time dateTime={props.arrivalHour}>{props.arrivalHour}</time>
       </div>
