7d1cb118d89f4713e9c5cf6fee22db8834efb040
varayac <victor.d.araya@gmail.com>
2024-02-26 09:02:19 -0300

    feat(backend): create Vehicles enpoints
    In this action the models are redefined and the enpoints corresponding to Vehicle are created

--- backend/database/seeders/20240217011139-demo-amenity.js
+++ backend/database/seeders/20240217011139-demo-amenity.js
@@ -8,164 +8,19 @@ module.exports = {
       'Amenities',
       [
         {
-          idVehicle: 1,
-          usb: true,
-          tv: true,
-          food: false,
-          wifi: true,
-          airConditioner: true
+          name: 'Cargador USB'
         },
         {
-          idVehicle: 2,
-          usb: true,
-          tv: true,
-          food: true,
-          wifi: true,
-          airConditioner: true
+          name: 'TV'
         },
         {
-          idVehicle: 3,
-          usb: true,
-          tv: true,
-          food: true,
-          wifi: true,
-          airConditioner: true
+          name: 'Comida'
         },
         {
-          idVehicle: 4,
-          usb: true,
-          tv: true,
-          food: false,
-          wifi: true,
-          airConditioner: true
+          name: 'WiFi'
         },
         {
-          idVehicle: 5,
-          usb: true,
-          tv: true,
-          food: true,
-          wifi: true,
-          airConditioner: true
-        },
-        {
-          idVehicle: 6,
-          usb: true,
-          tv: true,
-          food: false,
-          wifi: true,
-          airConditioner: true
-        },
-        {
-          idVehicle: 7,
-          usb: true,
-          tv: true,
-          food: true,
-          wifi: true,
-          airConditioner: true
-        },
-        {
-          idVehicle: 8,
-          usb: true,
-          tv: true,
-          food: false,
-          wifi: true,
-          airConditioner: true
-        },
-        {
-          idVehicle: 9,
-          usb: true,
-          tv: true,
-          food: true,
-          wifi: true,
-          airConditioner: true
-        },
-        {
-          idVehicle: 10,
-          usb: true,
-          tv: true,
-          food: true,
-          wifi: true,
-          airConditioner: true
-        },
-        {
-          idVehicle: 11,
-          usb: true,
-          tv: true,
-          food: false,
-          wifi: true,
-          airConditioner: true
-        },
-        {
-          idVehicle: 12,
-          usb: true,
-          tv: true,
-          food: true,
-          wifi: true,
-          airConditioner: true
-        },
-        {
-          idVehicle: 13,
-          usb: true,
-          tv: true,
-          food: true,
-          wifi: true,
-          airConditioner: true
-        },
-        {
-          idVehicle: 14,
-          usb: true,
-          tv: true,
-          food: false,
-          wifi: true,
-          airConditioner: true
-        },
-        {
-          idVehicle: 15,
-          usb: true,
-          tv: true,
-          food: true,
-          wifi: true,
-          airConditioner: true
-        },
-        {
-          idVehicle: 16,
-          usb: true,
-          tv: true,
-          food: false,
-          wifi: true,
-          airConditioner: true
-        },
-        {
-          idVehicle: 17,
-          usb: true,
-          tv: true,
-          food: true,
-          wifi: true,
-          airConditioner: true
-        },
-        {
-          idVehicle: 18,
-          usb: true,
-          tv: true,
-          food: false,
-          wifi: true,
-          airConditioner: true
-        },
-        {
-          idVehicle: 19,
-          usb: true,
-          tv: true,
-          food: true,
-          wifi: true,
-          airConditioner: true
-        },
-        {
-          idVehicle: 20,
-          usb: true,
-          tv: true,
-          food: true,
-          wifi: true,
-          airConditioner: true
+          name: 'Aire Acondicionado'
         }
       ],
       {}
