ce71ff6a96585e27d264a5cf756f51aaccbc1a20
redmor1 <lightz17@hotmail.com>
2024-02-28 18:41:56 -0300

    feat(frontend): :sparkles: Create component CreditCardForm

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -5,6 +5,11 @@ import StepsLayout from './layouts/StepsLayout.jsx';
 import SeatSelection from './pages/SeatSelection.jsx';
 import TravelSelection from './pages/TravelSelection.jsx';
 import TripReservation from './pages/ticket/TripReservation.jsx';
+// import UserLayout from './layouts/UserLayout.jsx';
+// import MyPurchases from './pages/auth/user/MyPurchases.jsx';
+// import UserSettings from './pages/auth/user/UserSettings.jsx';
+// import UserTravelDistance from './pages/auth/user/UserTravelDistance.jsx';
+import Payment from './pages/ticket/Payment.jsx';
 
 export const router = createBrowserRouter([
   {
@@ -39,7 +44,7 @@ export const router = createBrowserRouter([
       },
       {
         path: 'payment',
-        element: <p>Payment</p>
+        element: <Payment />
       }
     ]
   }
